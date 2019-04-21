2fbe9cd7c22cd5943186fedb15480ef9dc4f50d3
Alexandru Stoica <damagecs@gmail.com>
2019-04-21 21:17:54 +0300

    Fixed some typos and spaces

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,7 @@
 * Phone Book app
 * Type 1 to search by phone number
 * Type 2 to search by person's name
-* Type 3 to exit
+* Type 3 to save a contact
  */
 
 package com.company;
@@ -82,36 +82,35 @@ public class Main {
         try {
             Scanner in = new Scanner(System.in);
 
-
-            System.out.println("-----------------------------------");
+            System.out.println("---------------------------------------");
             System.out.println("Welcome to Phone Book v0.1 alpha");
-            System.out.println("-----------------------------------");
+            System.out.println("---------------------------------------");
             System.out.println("Press 1 to search a phone number");
-            System.out.println("-----------------------------------");
+            System.out.println("---------------------------------------");
             System.out.println("Press 2 to search a person's name");
-            System.out.println("-----------------------------------");
+            System.out.println("---------------------------------------");
             System.out.println("Press 3 to save a person's contact info");
-            System.out.println("-----------------------------------");
+            System.out.println("---------------------------------------");
 
             int choice = in.nextInt();
             in.nextLine();
 
             switch (choice) {
                 case 1:
-                    System.out.println("\nWhat phone number would you like to find?  (0040741159434)\n");
+                    System.out.println("\nWhat phone number would you like to find? (0040741159434)\n");
                     findNumber(in.nextLine());
                     break;
 
                 case 2:
-                    System.out.println("\nWhat is the person's name would you like to find? (FirstName)\n");
+                    System.out.println("\nWhat is the person's name would you like to find? (FirstName Lastname)\n");
                     findName(in.nextLine());
                     break;
 
                 case 3:
-                    System.out.println("\nWhat is the person's info you would like to save (LastName)");
+                    System.out.println("\nWhat is the person's info you would like to save (FirstName LastName)");
                     String name = in.nextLine();
 
-                    System.out.println("\n What is the phone number of the person you want to save? (0040741159434)");
+                    System.out.println("\nWhat is the phone number of the person you want to save? (0040741159434)");
                     long number = in.nextLong();
                     in.nextLine();
 
